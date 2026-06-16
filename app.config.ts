00e7abafeb6e72b07dd7758d3cbc87821647496d
Abhishek29041995 <43287331+Abhishek29041995@users.noreply.github.com>
2026-06-16 20:19:06 +0530

    quality fix

--- app.config.ts
+++ app.config.ts
@@ -1,11 +1,16 @@
 import dotenv from 'dotenv';
 import type { ExpoConfig } from 'expo/config';
 
+// Load default .env first
+dotenv.config();
+
 export default (): ExpoConfig => {
   const ENV = process.env.EXPO_PUBLIC_ENV ?? 'mock';
 
+  // Then load environment-specific file
   dotenv.config({
     path: `.env.${ENV}`,
+    override: true,
   });
 
   return {
